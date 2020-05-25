5847053287448a0674664dfdf6dfc09e8d7e4820
iBotamon <ibotamon@gmail.com>
2020-05-25 17:33:29 +0900

    commands to download BioELMo

--- download_models.sh
+++ download_models.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Download pretrained BioELMo
+
+bioelmo_dir=models/bioelmo
+bioelmo_options_path=${bioelmo_dir}/options.json
+bioelmo_model_path=${bioelmo_dir}/bioelmo.hdf5
+
+file_id=19sLZ1NhUtD_bMgTstSRWoVDx6Vm-T8Qt
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${file_id}" -o ${bioelmo_options_path}
+
+file_id=1CHRd5YQrt3ys64WfJkJR1KX72-2CaT4I
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${file_id}" -o ${bioelmo_model_path}
+
 # Download BioELMo + CRF EBMNLP model checkpoint
 
 ebmnlp_bioelmo_crf_checkpoint_path=models/ebmnlp_bioelmo_crf/ebmnlp_bioelmo_crf.ckpt
