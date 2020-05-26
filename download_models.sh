e2ecaf506989f7a44623805feda3bb9cdf8e4603
ibotamon <ibotamon@gmail.com>
2020-05-26 20:20:51 +0900

    Added vocab.txt download information & script

--- download_models.sh
+++ download_models.sh
@@ -5,6 +5,7 @@
 bioelmo_dir=models/bioelmo
 bioelmo_options_path=${bioelmo_dir}/biomed_elmo_options.json
 bioelmo_model_path=${bioelmo_dir}/bioelmo_elmo_weights.hdf5
+bioelmo_vocab_path=${bioelmo_dir}/vocab.txt
 
 file_id=19sLZ1NhUtD_bMgTstSRWoVDx6Vm-T8Qt
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
@@ -15,6 +16,12 @@ file_id=1CHRd5YQrt3ys64WfJkJR1KX72-2CaT4I
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${file_id}" -o ${bioelmo_model_path}
+
+file_id=15cXEVoRhUQ9oBnHVFP3nx6GQozczgxgP
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${file_id}" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=${file_id}" -o ${bioelmo_vocab_path}
+
 
 # Download BioELMo + CRF EBMNLP model checkpoint
 
--- models/bioelmo/README.txt
+++ models/bioelmo/README.txt
@@ -1,2 +1,5 @@
-Download BioELMo weights here from https://drive.google.com/file/d/1CHRd5YQrt3ys64WfJkJR1KX72-2CaT4I/view
-Download BioELMo options here from https://drive.google.com/file/d/19sLZ1NhUtD_bMgTstSRWoVDx6Vm-T8Qt/view
+Place BioELMo weights here (available at https://drive.google.com/file/d/1CHRd5YQrt3ys64WfJkJR1KX72-2CaT4I/view)
+
+Place BioELMo options here (available at https://drive.google.com/file/d/19sLZ1NhUtD_bMgTstSRWoVDx6Vm-T8Qt/view)
+
+Place BioELMo vocab here (available at https://drive.google.com/file/d/15cXEVoRhUQ9oBnHVFP3nx6GQozczgxgP/view)
